f18014c89de6b7f78f969df5ae369cb8bc436fa1
dessieoconnor <dessieoconnor@gmail.com>
2025-03-07 11:49:00 +1100

    Update des.php

--- des.php
+++ des.php
@@ -15,4 +15,10 @@ foreach ($file_db->query($sql) as $row) {
 
     echo $employee;
 }
-echo 'tetsing';
+$sql = 'SELECT * FROM employees WHERE employeeId = ' . $_GET['id'];
+
+foreach ($file_db->query($sql) as $row) {
+    $employee = $row['LastName'] . " - " . $row['Email'] . "\n";
+
+    echo $employee;
+}
